515832711e29aed27c285c20781b0cae9e371871
Wr2020 <comin_008@163.com>
2020-06-29 20:27:19 +0800

    jedis分片

---
+++ Review-2019/Redis/src/main/java/TestJedis.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.Test;
+import redis.clients.jedis.*;
+
+import java.util.ArrayList;
+
+public class TestJedis {
+
+    private static final String HOST = "192.168.116.129";
+
+    @Test
+    public void testJedis() {
+        Jedis jedis = new Jedis(HOST, 6379);
+        System.out.println(jedis.get("name"));
+        jedis.close();
+    }
+
+    @Test
+    public void testJedisPool() {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(200);
+
+        JedisPool pool = new JedisPool(config, HOST, 6379);
+
+        System.out.println(pool.getResource().get("name"));
+
+        pool.close();
+    }
+
+
+    @Test
+    public void testShard() {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(200);
+
+        ArrayList<JedisShardInfo> list = new ArrayList<JedisShardInfo>();
+        list.add(new JedisShardInfo(HOST, 6379));
+        list.add(new JedisShardInfo(HOST, 6380));
+        list.add(new JedisShardInfo(HOST, 6381));
+        ShardedJedisPool pool = new ShardedJedisPool(config,list);
+        for (int i = 0; i < 10; i++) {
+            pool.getResource().set("u" + i, "" + i);
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println( pool.getResource().get("u" + i));
+        }
+        pool.close();
+    }
+
+}
